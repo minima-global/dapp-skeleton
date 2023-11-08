91e2a09ec1f5cf60595bd1291bfc3ade21a7d289
Elias <eliasnemr@gmail.com>
2023-11-08 19:25:56 +0200

    Fix few tings

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { createHtmlPlugin } from "vite-plugin-html";
+import legacy from "@vitejs/plugin-legacy";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  let devEnv = "";
+  const env = Object.assign(
+    globalThis.process.env,
+    loadEnv(mode, globalThis.process.cwd())
+  );
+
+  if (mode === "development") {
+    devEnv = `
+      <script>
+        var DEBUG = "${env.VITE_DEBUG}" === 'true';
+        var DEBUG_HOST = "${env.VITE_DEBUG_HOST}";
+        var DEBUG_PORT = "${env.VITE_DEBUG_MDS_PORT}";
+        var DEBUG_UID = "${env.VITE_DEBUG_UID}";
+      </script>
+    `;
+  }
+
+  return defineConfig({
+    base: "",
+    build: {
+      outDir: "build",
+    },
+    plugins: [
+      react(),
+      legacy({
+        targets: ["defaults", "not IE 11", "Android >= 9"],
+      }),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            devEnv,
+          },
+        },
+      }),
+    ],
+  });
+};
