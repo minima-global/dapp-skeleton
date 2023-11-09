c9798a0bef7a6d645dba4082ad7e7d5b3cbaa819
Elias <eliasnemr@gmail.com>
2023-11-09 15:27:57 +0200

    Added decorators for tailwind in index.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,17 @@ function App() {
     <>
       <div>
         <a href="https://github.com/eliasnemr" target="_blank">
-          <img src={viteLogo} className="logo animate-spin" alt="Vite logo" />
+          <img
+            src={viteLogo}
+            className="logo animate-pulse inline"
+            alt="Vite logo"
+          />
         </a>
       </div>
-      <h1>MiniDapp Skeleton, (React TS + Vite + TailwindCSS)</h1>
+      <h1 className="text-lg text-blue-400">
+        MiniDapp Skeleton, <br />
+        (React TS + Vite + TailwindCSS)
+      </h1>
     </>
   );
 }
